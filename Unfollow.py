3d1ce1357895729a825167f3afd2f69eee104bfb
TomoyaFujita2016 <fujita.t.2016c@gmail.com>
2017-07-10 22:48:32 +0900

    Update

--- Unfollow.py
+++ Unfollow.py
@@ -1,16 +1,16 @@
 import byDestroy as byD
 import twitterApiSetup as tas
 import tweepy
+import byGoodUser as byG
 
 api = tas.tweetSetup()
 me = api.me()
-followers = byD.getFollowersIds(api)
 friends = byD.getFriendsIds(api, me.id)
 def byFollow(api, friend_id, lowerFFRatio, upperHashRatio, lowerTPDRatio, MyFriends):
     user = api.get_user(friend_id)
-    if(byG.byAlreadyFriend(api, friend_id, MyFriends)):
-        print("[ " + user.name + " ] is already followed!")
-        return False
+    #if(byG.byAlreadyFriend(api, friend_id, MyFriends)):
+        #print("[ " + user.name + " ] is already followed!")
+        #return False
     if(not byG.byFFRatio(api, friend_id, lowerFFRatio)):
         print("[ " + user.name + " ] is a passive user!")
         return False
@@ -25,10 +25,11 @@ def byFollow(api, friend_id, lowerFFRatio, upperHashRatio, lowerTPDRatio, MyFrie
         return False
     return True
 UnfollowCnt = 0
+print("Started Unfollowing!")
 for friend in friends:
-    if not byFollow(api, friend, 0.75, 0.5, 0.25, friends):
+    name = api.get_user(friend)
+    if not(byFollow(api, friend, 0.75, 0.5, 0.25, friends)):
         try:
-            name = api.get_user(friend)
             api.destroy_friendship(friend)
             UnfollowCnt += 1
             if UnfollowCnt == 80:
@@ -37,3 +38,5 @@ for friend in friends:
             print ("Successed in unfollowing " + name.name)
         except tweepy.error.TweepError:
             print("ERROR: FAILED TO UNFOLLOW " + name.name)
+    else:
+        print("Didn't Unfollow " + name.name)
