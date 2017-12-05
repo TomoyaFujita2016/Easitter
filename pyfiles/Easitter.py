b50acf7b740753af0be54d51c7f58df265edf7d3
TomoyaFujita <fujita.t.2016c@gmail.com>
2017-12-05 18:34:20 +0900

    delete some code

--- pyfiles/Easitter.py
+++ pyfiles/Easitter.py
@@ -11,8 +11,8 @@ import re
 class Easitter(object):
     CK = "warrm7a0cjWy62GbnjQRLUXtd"
     CS = "56CITHgkJyhx824WlYyM8lgp4sBE2M6j1bo4PfxXBY4Oti1Cz5"
-    AT = "883630081473617921-5EHbdHAPn7MURLhFtD0uewHvMQPs3X2"
-    AS = "0ObJeBCW2yszFr8elB8PDGxi24JEoVpxmTSAbELucy9oe"
+    AT = ""
+    AS = ""
 
     def __init__(self, CK=CK,CS=CS,AT=AT,AS=AS):
         self.SAMPLE_NUM = 50
