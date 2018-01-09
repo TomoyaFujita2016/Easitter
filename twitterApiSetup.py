531f4492cb730d1b9011e865a1d389919b65067a
TomoyaFujita <fujita.t.2016c@gmail.com>
2018-01-09 09:26:42 +0900

    changed api key

--- twitterApiSetup.py
+++ twitterApiSetup.py
@@ -19,9 +19,9 @@ def readPklFile(path):
         
 
 def tweetSetup():
-    CONSUMER_KEY = 'warrm7a0cjWy62GbnjQRLUXtd'
-    CONSUMER_SECRET = '56CITHgkJyhx824WlYyM8lgp4sBE2M6j1bo4PfxXBY4Oti1Cz5'
-    pklPath = "./.Data/pklData.pkl"
+    CONSUMER_KEY = 'KINHRDJKDjXxjf0Y9WMPuE09q'
+    CONSUMER_SECRET = 'UwVaMjr6bEOK1WquNDOMCsSmZc2phVangJkvy7TU3zYXjsrKVH'
+    pklPath = "./AC_PATH/pklData.pkl"
     
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     data = readPklFile(pklPath)
