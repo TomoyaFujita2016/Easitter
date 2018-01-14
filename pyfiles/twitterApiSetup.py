526efa2144fb3672c52328eec445fb1981c375c1
TomoyaFujita <fujita.t.2016c@gmail.com>
2018-01-14 20:20:22 +0900

    added new authorize system

--- pyfiles/twitterApiSetup.py
+++ pyfiles/twitterApiSetup.py
@@ -2,20 +2,59 @@
 import tweepy
 import pickle as pkl
 import os
+import random
+import shutil as sh
 
 ACCESS_PATH = "AC_PATH/accessCodes.pkl"
+C_PATH = ".CSCK.txt"
+NUM_PATH  = ".number.pkl"
+FIRST_PATH = ".byFirst"
+
+def initialize():
+    if byFirstExecution():
+        if os.path.exists(ACCESS_PATH):
+            sh.rmtree(ACCESS_PATH)
+        if os.path.exists(NUM_PATH):
+            os.remove(NUM_PATH)
 
 def encryptCode(code):
     return code.encode('utf8').encode('base64_codec').encode('rot_13')
 def decryptCode(code):
     return code.decode('rot_13').decode('base64_codec').decode('utf8')
 
-def getAccessKeys():
-    CK = "fLhoQoaXRlR7cucVWlrNfM9ih"
-    CS = "4uaqsQ5aqk5u5j8Se8LFnvvbAoHfZQm1LXqHsjM9GIzuaAaTZQ"
-    ACCESS_CODES = [CK, CS]
-    auth = tweepy.OAuthHandler(CK, CS)
+def getCs():
+    CS = []
+    with open(C_PATH, "r") as f:
+        for line in f:
+            CS.append(line.replace("\n", ""))
+    return CS
+
+def byFirstExecution():
+    if os.path.exists(FIRST_PATH):
+        os.remove(FIRST_PATH)
+        return True
+    return False
+
+def selectCS():
+    CS = getCs()
+    if os.path.exists(NUM_PATH):
+        with open(NUM_PATH, "rb") as f:
+            rand = pkl.load(f)
+            print("CSCK: " + str(rand))
+        return CS[rand].split(",")
     
+    rand = random.randint(0, len(CS)-1)
+    print("CSCK: " + str(rand))
+    with open(NUM_PATH, "wb") as f:
+        pkl.dump(rand, f)
+    return CS[rand].split(",")
+        
+
+def getAccessKeys():
+    initialize()
+    ACCESS_CODES = selectCS()
+    auth = tweepy.OAuthHandler(ACCESS_CODES[0], ACCESS_CODES[1])
+    print("auth done") 
     if not os.path.exists(ACCESS_PATH):
         redirect_url = auth.get_authorization_url()
         print ('Get your verification code from:' + redirect_url)
